ecd8f519ab0b1e9c0cce0e06ab8cecdcba8b24ea
Toke Eskildsen <toes@kb.dk>
2020-01-21 14:52:57 +0100

    Add Mockito sample

--- src/test/java/dk/kb/template/HelloWorldTest.java
+++ src/test/java/dk/kb/template/HelloWorldTest.java
@@ -1,11 +1,14 @@
 package dk.kb.template;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class HelloWorldTest {
@@ -21,7 +24,41 @@ public class HelloWorldTest {
     
     }
     
-    
+    @Tag("fast")
+    @Test
+    @DisplayName("Mockito demonstration")
+    public void mockito() throws MalformedURLException, ClassNotFoundException {
+        // Mockito makes it easy to create mocked versions of classes.
+        // Basically it creates an empty shell from the class and lets the test-writer fill in the methods
+        // used by the test. The class constructor is bypassed. The class that is mocked here is ClassLoader,
+        // to demonstrate that it is possible to mock very complex classes
+        ClassLoader mockedLoader = mock(ClassLoader.class);
+
+        // Methods that are not explicitly mocked returns null
+        assertNull(mockedLoader.getName());
+
+        // We can assign a result to a method
+        when(mockedLoader.getName()).thenReturn("Mockito ClassLoader");
+        assertEquals("Mockito ClassLoader", mockedLoader.getName());
+
+        // We can assign conditionals to methods that takes arguments
+        when(mockedLoader.getResource(eq("example"))).thenReturn(new URL("http://example.org/"));
+        when(mockedLoader.getResource(eq("KB"))).thenReturn(new URL("https://www.kb.dk/"));
+        assertNull(mockedLoader.getResource("Undefined"));
+        assertEquals(new URL("http://example.org/"), mockedLoader.getResource("example"));
+        assertEquals(new URL("https://www.kb.dk/"), mockedLoader.getResource("KB"));
+
+        // Arguments need not be defined
+        when(mockedLoader.getResource(anyString())).thenReturn(new URL("http://statsbiblioteket.dk/"));
+        assertEquals(new URL("http://statsbiblioteket.dk/"), mockedLoader.getResource("randomString"));
+
+        // The result can rely on the input
+        when(mockedLoader.getResource(anyString())).thenAnswer(
+                input -> new URL("http://" + input.getArgument(0) + ".example.org/"));
+        assertEquals(new URL("http://foo.example.org/"), mockedLoader.getResource("foo"));
+    }
+
+
     @Tag("slow")
     @Test
     public void sleeper() {
